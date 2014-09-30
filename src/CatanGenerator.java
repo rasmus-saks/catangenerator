4a74c0bef5f06ec7143000b259ce6e9e084bb05f
Rasmus Saks <bogeyman.est@gmail.com>
2014-09-30 14:51:08 +0300

    Initial commit

---
+++ src/CatanGenerator.java
@@ -0,0 +1,37 @@
+/*
+    Board hexes:
+        19x Land Hexes:
+            4x  Wood
+            4x  Crops
+            4x  Sheep
+            3x  Ore
+            3x  Clay
+            1x  Desert
+        18x Sea Hexes:
+            4x  3:1 Trade
+            1x  2:1 Wood
+            1x  2:1 Sheep
+            1x  2:1 Crops
+            1x  2:1 Ore
+            9x  Sea
+
+    Dice roll probabilities (of 36 different rolls):
+        2:  1
+        3:  2
+        4:  3
+        5:  4
+        6:  5
+        7:  6
+        8:  5
+        9:  4
+        10: 3
+        11: 2
+        12: 1
+ */
+public class CatanGenerator {
+    public static void main(String[] args) {
+        System.out.println("Katani Asustajate mänguvälja generaator");
+        System.out.println("      Autorid: Rasmus Saks ja AL William Tammsaar");
+    }
+
+}
