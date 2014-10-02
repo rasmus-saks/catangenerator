aed4d2f2f67407cdee67561e6aee8cb8f381d1dd
Rasmus Saks <bogeyman.est@gmail.com>
2014-10-02 16:19:59 +0300

    Change Generator class to be abstract

--- src/Generator.java
+++ src/Generator.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class Generator {
+public abstract class Generator {
     /**
      * Generate a list of hexes. {@link CatanGenerator} contains some useful static fields to help with this.
      * @return The generated list of hexes
@@ -9,7 +9,5 @@ public class Generator {
      * @see CatanGenerator#ALL_LOCATIONS
      * @see CatanGenerator#ALL_NUMBERS
      */
-    public List<Hex> generateHexes() {
-        return null;
-    }
+    public abstract List<Hex> generateHexes();
 }
