7580ef4bd389ea079c8471d080ceae578ba5792a
Rasmus Saks <bogeyman.est@gmail.com>
2014-11-09 19:46:45 +0200

    Remove some unnecessary code from BoardRenderer

--- src/BoardRenderer.java
+++ src/BoardRenderer.java
@@ -1,13 +1,3 @@
-import java.util.List;
-
 public interface BoardRenderer {
-    public abstract void render();
-
-    default Hex getHexAt(List<Hex> hexes, Location loc) {
-        for (Hex h : hexes) {
-            if(h.getLoc().equals(loc))
-                return h;
-        }
-        return null;
-    }
+    public void render();
 }
